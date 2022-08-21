9374060253dd28fde862dcadd32e9aaaf8b0e551
AndrewKim330 <rendezvous330@gmail.com>
2022-08-21 16:06:53 +0900

    feat: Study basic of GoLang - constant and varaible

--- main.go
+++ main.go
@@ -10,5 +10,18 @@ import (
 func main() {
 	fmt.Printf("Hello world! \n")
 	something.SayHello()
-	//something.sayBye() expected to error
+	//something.sayBye() -> expected to error
+
+	const name = "andrew"
+	//name = "good" -> expected to error
+	//const name string = "andrew"
+	//const name bool = "andrew" -> expected to error
+	fmt.Println(name)
+
+	//var name1 = "andrew"
+	//var name1 string = "andrew"
+	name1 := "andrew"
+	name1 = "good"
+	fmt.Printf(name1)
+
 }
