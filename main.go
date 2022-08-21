a8db8157689cc3009ec710020b3486a096dbbed0
AndrewKim330 <rendezvous330@gmail.com>
2022-08-21 15:46:57 +0900

    feat: Construct initial structure of Go

---
+++ main.go
@@ -0,0 +1,14 @@
+// 220821 re-construct repository
+
+package main
+
+import (
+	"fmt"
+	"go_study/something"
+)
+
+func main() {
+	fmt.Printf("Hello world! \n")
+	something.SayHello()
+	//something.sayBye() expected to error
+}
---
+++ something/something.go
@@ -0,0 +1,11 @@
+package something
+
+import "fmt"
+
+func SayHello() {
+	fmt.Printf("Hello")
+}
+
+func sayBye() {
+	fmt.Printf("Bye")
+}
