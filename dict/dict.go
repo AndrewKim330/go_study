4622756feb943c3d69acfe614780baa61fa1b720
AndrewKim330 <rendezvous330@gmail.com>
2022-08-31 19:05:21 +0900

    feat: Study basic of GoLang - Making dictionary, making add method

--- dict/dict.go
+++ dict/dict.go
@@ -15,3 +15,23 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 	return "", errNotFound
 }
+
+// 220831
+var errWordExists = errors.New("That word already exists")
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	//switch err {
+	//case errNotFound:
+	//	d[word] = def
+	//case nil:
+	//	return errWordExists
+	//}
+	if err == errNotFound {
+		d[word] = def
+	} else if err == nil {
+		return errWordExists
+	}
+	return nil
+}
--- main.go
+++ main.go
@@ -6,13 +6,27 @@ import (
 )
 
 func main() {
-	dictionary := dict.Dictionary{"first": "First word"}
-	// fmt.Println(dictionary["first"]) -> not fancy....
-	definition, err := dictionary.Search("first")
-	// definition, err := dictionary.Search("second") // expect to print error
+	//dictionary := dict.Dictionary{"first": "First word"}
+	//// fmt.Println(dictionary["first"]) -> not fancy....
+	//definition, err := dictionary.Search("first")
+	//// definition, err := dictionary.Search("second") // expect to print error
+	//if err != nil {
+	//	fmt.Println(err)
+	//} else {
+	//	fmt.Println(definition)
+	//}
+
+	word := "hello"
+	definition := "greeting"
+	dictionary := dict.Dictionary{}
+	err := dictionary.Add(word, definition)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
 	}
+	hello, err := dictionary.Search(word)
+	fmt.Println("found: ", word, "definition: ", hello)
+
+	err2 := dictionary.Add(word, definition)
+	fmt.Println(err2)
+
 }
