fe0837e91674608112b98bd1c7e32017c4e0b400
AndrewKim330 <rendezvous330@gmail.com>
2022-09-01 20:09:58 +0900

    feat: Study basic of GoLang - Making url checker, slow(synchronous) url checker

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ import (
 )
 
 func main() {
+	//var results map[string]string // expected to panic error
+	//var results = map[string]string{} // would be work well
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -18,10 +21,19 @@ func main() {
 		"https://www.instagram.com/",
 		"https://www.facebook.com/",
 	}
-
+	//results["hello"] = "hello"
 	for _, url := range urls {
 		//fmt.Println(url)
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	//fmt.Println(results)
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
@@ -31,6 +43,7 @@ func hitURL(url string) error {
 	fmt.Println("Checking: ", url)
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errRequestFailed
 	}
 	return nil
