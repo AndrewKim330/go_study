27829f700788dbee769c9a809ebcb1f55d0900df
AndrewKim330 <rendezvous330@gmail.com>
2022-09-06 18:58:06 +0900

    feat: Study basic of GoLang - Making get pages from Indeed, using goquery

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"log"
 	"net/http"
 )
 
 // var baseURL string = "https://kr.indeed.com/jobs?q=python&l=&ts=1662298375496&rq=1&rsIdx=0&fromage=last&newcount=1541&vjk=1015284880e2ff62"
-var baseURL string = "https://www.naver.com"
+var baseURL = "https://kr.indeed.com/?from=gnav-jobsearch--indeedmobile"
+
+//var baseURL string = "https://www.naver.com"
 
 func main() {
 	getPages()
@@ -23,7 +26,17 @@ func getPages() int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	doc.find(".pagination").Each()
+	fmt.Println(doc)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+
+		//html, err := s.Html()
+		//if err != nil {
+		//	log.Fatalln(err)
+		//}
+		//fmt.Println(html)
+		fmt.Println(s.Html())
+	})
 
 	return 0
 }
